322801993f5018364c48358d99eb50c0ef40170e
Joshua Tan <josh.tan.tann@gmail.com>
2020-10-27 16:48:09 -0700

    Implement and test DayListItem component
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,9 @@ import classNames from 'classnames';
 import 'components/Button.scss';
 
 export default function Button(props) {
-  const buttonClass = classNames("button", {
-    "button--confirm": props.confirm,
-    "button--danger": props.danger
+  const buttonClass = classNames('button', {
+    'button--confirm': props.confirm,
+    'button--danger': props.danger
   });
 
   return (
---
+++ src/components/DayListItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import 'components/DayListItem.scss';
+
+export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props;
+
+  const dayListItemClass = classNames('day-list__item', {
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots <= 0
+  });
+
+  const formatSpots = spotsRemaining => {
+    if (spotsRemaining === 0) {
+      return "no spots remaining";
+    } else if (spotsRemaining === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${spots} spots remaining`;
+    }
+  }
+
+  return (
+    <li 
+      onClick={() => setDay(name)}
+      className={dayListItemClass}
+    >
+      <h2 className='text--regular'>{name}</h2> 
+      <h3 className='text--light'>{formatSpots(spots)}</h3>
+    </li>
+  );
+}
