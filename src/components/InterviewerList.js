b1cfb4bf919eca122b79923fa85356ab761dd6f3
Joshua Tan <josh.tan.tann@gmail.com>
2020-10-27 20:42:15 -0700

    Fix typo
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,12 +3,12 @@ import React from 'react';
 import 'components/InterviewerList.scss';
 import InterviewerListItem from 'components/InterviewerListItem'
 
-export default function DayList(props) {
+export default function InterviewerList(props) {
   const { interviewers, interviewer, setInterviewer } = props;
 
   const interviewerListItems = interviewers.map((eachInterviewer, index) => (
     <InterviewerListItem
-      key={index}
+      id={eachInterviewer.id}
       name={eachInterviewer.name}
       avatar={eachInterviewer.avatar}
       selected={eachInterviewer.id === interviewer}
