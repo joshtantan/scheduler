49f36cdd7d5a8276d1f09fb76a78034ab712ebd4
Joshua Tan <josh.tan.tann@gmail.com>
2020-10-30 23:10:35 -0700

    Implement appointment fetching from API
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,7 +7,8 @@ import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay } from "../helpers/selectors";
 
-const daysURL = `http://localhost:8001/api/days`
+const daysURL = "http://localhost:8001/api/days";
+const appointmentsURL = "http://localhost:8001/api/appointments";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -17,25 +18,26 @@ export default function Application(props) {
   });
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
-
-  const appointmentItems = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
-    appointmentItems.map(appointment => (
-      <Appointment 
-        key={appointment.id} 
-        {...appointment}
-      />
-    ));
-  }, [appointmentItems]);
+    const daysPromise = axios.get(daysURL);
+    const appointmentsPromise = axios.get(appointmentsURL);
 
-  useEffect(() => {
-    axios.get(daysURL)
-    .then(res => {
-      setDays(res.data);
-    });
+    Promise.all([daysPromise, appointmentsPromise])
+      .then(([res1, res2]) => {
+        const days = res1.data;
+        const appointments = res2.data;
+
+        setState(prev => ({ ...prev, days, appointments }));
+      });
   }, []);
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day).map(appointment => (
+    <Appointment 
+      key={appointment.id} 
+      {...appointment}
+    />
+  ));
 
   return (
     <main className="layout">
@@ -60,7 +62,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointmentItems}
+        {dailyAppointments}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
