a113fecb8381433502f11505eec07eda66ad28db
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-01 12:29:34 -0800

    Uncomment getInterview test suite
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -94,21 +94,21 @@ test("getInterviewersForDay returns an empty array when the day is not found", (
   expect(result.length).toEqual(0);
 });
 
-// test("getInterview returns an object with the interviewer data", () => {
-//   const result = getInterview(state, state.appointments["3"].interview);
-//   expect(result).toEqual(
-//     expect.objectContaining({
-//       student: expect.any(String),
-//       interviewer: expect.objectContaining({
-//         id: expect.any(Number),
-//         name: expect.any(String),
-//         avatar: expect.any(String)
-//       })
-//     })
-//   );
-// });
+test("getInterview returns an object with the interviewer data", () => {
+  const result = getInterview(state, state.appointments["3"].interview);
+  expect(result).toEqual(
+    expect.objectContaining({
+      student: expect.any(String),
+      interviewer: expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String),
+        avatar: expect.any(String)
+      })
+    })
+  );
+});
 
-// test("getInterview returns null if no interview is booked", () => {
-//   const result = getInterview(state, state.appointments["2"].interview);
-//   expect(result).toBeNull();
-// });
+test("getInterview returns null if no interview is booked", () => {
+  const result = getInterview(state, state.appointments["2"].interview);
+  expect(result).toBeNull();
+});
