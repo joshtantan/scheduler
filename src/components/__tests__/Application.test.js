fd382754a777043dd84f7710a1316ce463174986
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-04 20:58:18 -0800

    Add more tests to Application
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,19 +1,37 @@
 import React from "react";
-import axios from "axios";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByAltText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
 describe("Application", () => {
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  it("changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
 
-    return waitForElement(() => getByText("Monday")).then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    await waitForElement(() => getByText("Monday"));
+
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { getByText, getAllByAltText, getByPlaceholderText, getAllByRole } = render(<Application />);
+
+    await waitForElement(() => getByText("Archie Cohen"));
+
+    const [add1] = getAllByAltText("Add");
+    fireEvent.click(add1);
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
     });
+
+    const [interviewerListItem1] = getAllByRole("listitem");
+    fireEvent.click(interviewerListItem1);
+
+    // expect(getByText("Monday")).toBeInTheDocument();
+    // expect(getByText("no spots remaining")).toBeInTheDocument();
   });
 });
